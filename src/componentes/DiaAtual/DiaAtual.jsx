a13f413f74f72245a1e5a6fadfe34cafe1b2f3d7
ROBERT VINÍCIUS NAZARIO <robert_nazario@estudante.sesisenai.org.br>
2024-10-09 22:18:22 -0300

    first commit

---
+++ src/componentes/DiaAtual/DiaAtual.jsx
@@ -0,0 +1,10 @@
+/*2. Crie um componente chamado 'DiaAtual' que mostra o texto: "15" em uma <div> com o fundo azul, texto centralizado na cor branco*/
+
+const DiaAtual = () => {
+    return (
+        <>
+        <div className='diaAtual'>Dia Atual: 15</div>
+        </>
+    );
+};
+export default DiaAtual;
